48ab2a17311e27f7cd0ee2e7529de4da614c8cce
kstoqnov <kstoqnov@gmail.com>
2019-10-28 23:45:31 +0200

    add footer and tests

---
+++ src/app_footer_components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Footer = () => {
+
+    return (
+
+        <>
+
+            <footer>
+
+                <h4> This is Footer </h4>
+
+
+            </footer>
+
+
+        </>
+
+
+    );
+
+
+};
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app_components/App';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
