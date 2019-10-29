76c671a9c1cbb9028d4e9cb80de55ae6d6a5b4e4
kstoqnov <kstoqnov@gmail.com>
2019-10-30 00:41:54 +0200

    add html structure

---
+++ src/app_header_components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+
+        <header> this is header </header>
+
+
+    );
+
+}
+
+export default Header;
---
+++ src/app_main_components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main = () => {
+
+    return(
+
+        <main> This is Main </main>
+
+
+    );
+
+
+}
+
+export default Main;
