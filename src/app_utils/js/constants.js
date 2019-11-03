12b7d626fa500fc95a2349135461a7933b7558ed
kstoqnov <kstoqnov@gmail.com>
2019-11-04 01:17:22 +0200

    img proba

--- src/app_utils/js/constants.js
+++ src/app_utils/js/constants.js
@@ -1,8 +1,7 @@
-import no_image from '../../img/no_image';
 
 export const AUTH_TOKEN = 'auth-token';
 export const THEME = 'theme';
-export const MISSING_IMG ={no_image};
+export const MISSING_IMG ='src/img/no_image.jpg';
 export const ALL_EPISODES = 10;
 export const FIVE_CHARACTERS = 5;
 export const TWELVE_CHARACTERS = 12;
