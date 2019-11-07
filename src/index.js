a8e6d344a039d416255623255ff1e91f923a0758
kstoqnov <kstoqnov@gmail.com>
2019-11-07 14:23:54 +0200

    add custom name for setup apollo client

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-boost';
 import { setContext } from 'apollo-link-context';
 import { THEME } from './app_utils/js/constants';
-import { typeDefs} from './app_type_definitions/typeDefs';
+import { typeDefs } from './app_type_definitions/typeDefs';
 import { defaultOptions } from './app_utils/js/defaultOptionsApolloClient';
 
 const apiLink = createHttpLink({
@@ -32,6 +32,9 @@ const client = new ApolloClient({
   link: authLink.concat(apiLink),
   cache,
   typeDefs,
+  name: 'react-web-client',
+  version: '1.3',
+  queryDeduplication: false,
   defaultOptions
 });
 
